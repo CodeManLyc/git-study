476e68ef4bb66065efca4bc6a5a4d294f42762ff
CodeManLyc <2571832696@qq.com>
2023-04-15 16:55:20 +0800

    master-commit1

---
+++ src/main/java/com/itheima/GitTest.java
@@ -0,0 +1,18 @@
+package com.itheima;
+
+/**
+ * 测试Git入门
+ *
+ * @Author Lyc
+ * @Date 2023/4/15 13:12
+ * @Description: 测试Git入门
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git4 hot-fix-test");
+        System.out.println("测试分支冲突");
+        System.out.println("hello,hot-fix，master也修改，出现分支冲突");
+    }
+}
